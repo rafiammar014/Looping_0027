cf261630e7cbd0b243afced83ce1bd2709dfa81e
rafiammar014 <rafiammar014@gmail.com>
2026-03-31 18:11:44 +0800

    implementasi perulangan FOR untuk mencetak teks Teknologi Informasi UMY sebanyak 5 kali

--- looping.cpp
+++ looping.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+include <iostream>
+#include <ctime>
 using namespace std;
 
 int main() {
-    return 0;
-}
+    int x;
+
+    // Perulangan dengan FOR
+    // Mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
