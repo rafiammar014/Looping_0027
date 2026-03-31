ef9d15497fc6f9169aa1734b4644b7ad8da34a3b
rafiammar014 <rafiammar014@gmail.com>
2026-03-31 17:54:56 +0800

    Fix output procedure formatting and ensure proper main function structure

--- switchCase.cpp
+++ switchCase.cpp
@@ -46,3 +46,11 @@ void output() {
     cout << "Status Kuota    : " << statusKuota << endl;
     cout << "----------------------------------" << endl;
 }
+
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
