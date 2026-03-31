4d9f8668d4992f462fe4ff7896992c0b87d4bd8e
rafiammar014 <rafiammar014@gmail.com>
2026-03-31 18:23:54 +0800

    implementasi logika AND (&&) untuk pengecekan syarat kelulusan ganda

--- looping.cpp
+++ looping.cpp
@@ -87,4 +87,27 @@ if ((rerata >= 60) || (nilM >= 70)) {
     cout << "Status kelulusan = " << status << ", dengan nilai rata rata = " << rerata << endl;
 
 }
+
+Logika AND
+#include <iostream>
+using namespace std;
+
+int main() {
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+if ((rerata >= 60) && (nilM >= 70)) {
+        status = "Lulus";
+    } else {
+        status = "Tidak lulus";
+    }
+    
+    cout << "Status kelulusan = " << status << ", dengan nilai rata rata = " << rerata << endl;
+
+}
     
